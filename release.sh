4adf54f389a070572e398ec8ed83be6e0e7b3a58
Prabhat Sharma <hi.prabhat@gmail.com>
2019-07-10 02:52:36 -0700

    fixed release bug

--- release.sh
+++ release.sh
@@ -34,35 +34,7 @@ cd release/$TAG/windows && zip ecommerce-windows-amd64.zip * && cd ../../../
 
 # # create a release
 hub release create \
-    -a release/$TAG/ecommerce-darwin-amd64.zip \
-    -a release/$TAG/ecommerce-linux-amd64.zip \
-    -a release/$TAG/ecommerce-windows-amd64.zip \
+    -a release/$TAG/darwin/ecommerce-darwin-amd64.zip \
+    -a release/$TAG//linux/ecommerce-linux-amd64.zip \
+    -a release/$TAG/windows/ecommerce-windows-amd64.zip \
     -m "$TAG" $TAG
-
-
-# mkdir binaries/releases/$TAG
-
-# # darwin
-# rm -rf binaries/darwin/
-# env GOOS=darwin GOARCH=amd64 go build -o binaries/darwin/eksuser
-# zip binaries/releases/$TAG/eksuser-darwin-amd64.zip binaries/darwin/eksuser
-
-# # linux
-# rm -rf binaries/linux/
-# env GOOS=linux GOARCH=amd64 go build -o binaries/linux/eksuser
-# zip binaries/releases/$TAG/eksuser-linux-amd64.zip binaries/linux/eksuser
-
-# # windows
-# rm -rf binaries/windows/
-# env GOOS=windows GOARCH=amd64 go build -o binaries/windows/eksuser.exe
-# zip binaries/releases/$TAG/eksuser-windows-amd64.zip binaries/windows/eksuser.exe
-
-# # copy to current path
-# # cp binaries/darwin/eksuser ~/bin/
-
-# # create a release
-# hub release create \
-#     -a binaries/releases/$TAG/eksuser-darwin-amd64.zip \
-#     -a binaries/releases/$TAG/eksuser-linux-amd64.zip \
-#     -a binaries/releases/$TAG/eksuser-windows-amd64.zip \
-#     -m "$TAG" $TAG
