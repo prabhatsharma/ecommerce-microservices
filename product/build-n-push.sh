6f672d84d6c2bb94cff0486e736b24b8f273ba33
Prabhat Sharma <hi.prabhat@gmail.com>
2019-07-21 09:34:06 -0700

    fixed namespace issues, versions and updated dockerfiles

--- product/build-n-push.sh
+++ product/build-n-push.sh
@@ -2,4 +2,5 @@
 
 docker build . -t hiprabhat/ecommerce-product:v1
 docker push hiprabhat/ecommerce-product:v1
-
+docker tag hiprabhat/ecommerce-product:v1 hiprabhat/ecommerce-product:latest
+docker push hiprabhat/ecommerce-product:latest
