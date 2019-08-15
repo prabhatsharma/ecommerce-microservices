630c4319b63d56a5b1bb1d904cec1021ff90c9bd
Prabhat Sharma <hi.prabhat@gmail.com>
2019-08-14 21:18:02 -0700

    added helm tutorial details

--- person/build-n-push.sh
+++ person/build-n-push.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-docker build . -t hiprabhat/ecommerce-person:v1
-docker push hiprabhat/ecommerce-person:v1
-docker tag hiprabhat/ecommerce-person:v1 hiprabhat/ecommerce-person:latest
+docker build . -t hiprabhat/ecommerce-person:v2 -t hiprabhat/ecommerce-person:latest
+docker push hiprabhat/ecommerce-person:v2
 docker push hiprabhat/ecommerce-person:latest
